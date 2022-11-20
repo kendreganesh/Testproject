b0172c97d281062b55f1aae6f81bde23565e6491
Mtronics Computers <Mtronics Computers@DESKTOP-AEPDS9P>
2022-11-20 23:08:42 +0530

    new class added

---
+++ practice _All/src/Array/Git.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class Git {
+	 public static void staticCasting() {
+		 System.out.println("static super class");
+	 }
+	 public void Demo() {
+		 System.out.println("non static Demo super");
+	 }
+	 public void set() {
+		 System.out.println("non static set  super class ");
+	 }
+	}
+
+
